6b6e41f9d2c22153773e1219a147e3a11830630d
hahs <alexandre_2420@outlook.com>
2022-10-02 15:56:59 -0500

    update post implementation

--- src/app/posts/edit-post/edit-post.component.ts
+++ src/app/posts/edit-post/edit-post.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.state';
 import { getPostById } from '../state/posts.selector';
 import { Post } from '../../models/posts.models';
+import { updatePost } from '../state/posts.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-post',
   templateUrl: './edit-post.component.html',
   styleUrls: ['./edit-post.component.css'],
 })
-export class EditPostComponent implements OnInit {
+export class EditPostComponent implements OnInit, OnDestroy {
   post!: Post;
   postForm!: FormGroup;
+  postSubscription!: Subscription;
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -29,10 +33,12 @@ export class EditPostComponent implements OnInit {
       //   this.post = data!;
       //   this.createForm();
       // });
-      this.store.select(getPostById({ id })).subscribe((data) => {
-        this.post = data!;
-        this.createForm();
-      });
+      this.postSubscription = this.store
+        .select(getPostById({ id }))
+        .subscribe((data) => {
+          this.post = data!;
+          this.createForm();
+        });
     });
   }
 
@@ -65,6 +71,22 @@ export class EditPostComponent implements OnInit {
   }
 
   onUpdatePost() {
-    console.log(this.postForm.value);
+    if (this.postForm.invalid) {
+      return;
+    }
+
+    const title = this.postForm.value.title;
+    const description = this.postForm.value.description;
+
+    const post = { id: this.post.id, title, description };
+
+    this.store.dispatch(updatePost({ post }));
+    this.router.navigate(['posts']);
+  }
+
+  ngOnDestroy(): void {
+    if (this.postSubscription) {
+      this.postSubscription.unsubscribe();
+    }
   }
 }
--- src/app/posts/state/posts.reducer.ts
+++ src/app/posts/state/posts.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { addPost } from './posts.actions';
+import { addPost, updatePost } from './posts.actions';
 import { initialState, PostsState } from './posts.state';
 
 const _postsReducer = createReducer(
@@ -8,6 +8,13 @@ const _postsReducer = createReducer(
     let post = { ...action.post };
     post.id = (state.posts.length + 1).toString();
     return { ...state, posts: [...state.posts, post] };
+  }),
+  on(updatePost, (state, action) => {
+    const updatePosts = state.posts.map((p) => {
+      return action.post.id === p.id ? action.post : p;
+    });
+
+    return { ...state, posts: updatePosts };
   })
 );
 
