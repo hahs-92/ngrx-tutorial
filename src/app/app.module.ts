9fb9ba09403fc7ca7891bedcf8e5eeb81970b5e6
hahs <alexandre_2420@outlook.com>
2022-10-03 08:31:22 -0500

    implememt lazy-loading routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter/counter.component';
-import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
-import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { HomeComponent } from './home/home.component';
 
 //redux
@@ -16,27 +13,12 @@ import { StoreModule } from '@ngrx/store';
 //--reducer
 import { appReducer } from './store/app.state';
 
-import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { PostsListComponent } from './posts/posts-list/posts-list.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { AddPostComponent } from './posts/add-post/add-post.component';
-import { EditPostComponent } from './posts/edit-post/edit-post.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent,
-    CounterOutputComponent,
-    CounterButtonsComponent,
-    CustomCounterInputComponent,
-    HomeComponent,
-    HeaderComponent,
-    PostsListComponent,
-    AddPostComponent,
-    EditPostComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, HeaderComponent],
   //agregamos los reducers, puede ser mas de uno
   imports: [
     BrowserModule,
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter/counter.component';
-import { PostsListComponent } from './posts/posts-list/posts-list.component';
-import { AddPostComponent } from './posts/add-post/add-post.component';
-import { EditPostComponent } from './posts/edit-post/edit-post.component';
 
 const routes: Routes = [
   {
@@ -13,15 +9,13 @@ const routes: Routes = [
   },
   {
     path: 'counter',
-    component: CounterComponent,
+    loadChildren: () =>
+      import('./counter/counter.module').then((m) => m.CounterModule),
   },
   {
     path: 'posts',
-    component: PostsListComponent,
-    children: [
-      { path: 'add', component: AddPostComponent },
-      { path: 'edit/:id', component: EditPostComponent },
-    ],
+    loadChildren: () =>
+      import('./posts/posts.module').then((m) => m.PostsModule),
   },
 ];
 
